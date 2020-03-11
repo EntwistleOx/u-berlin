37b9da8d522add614b07e3cfdf473442af29d42c
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-11 02:22:08 -0300

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,18 +8,17 @@ class MyDocument extends Document {
         <body>
           <Main />
           {/* Placed at the end of the document so the pages load faster */}
-          <script src='assets/js/jquery.js'></script>
+          {/* <script src='assets/js/jquery.js'></script>
           <script src='assets/js/jquery.easing.1.3.js'></script>
           <script src='assets/materialize/js/materialize.min.js'></script>
           <script src='assets/js/bootstrap.min.js'></script>
           <script src='assets/js/jquery.fancybox.pack.js'></script>
           <script src='assets/js/jquery.fancybox-media.js'></script>
           <script src='assets/js/animate.js'></script>
-          {/* Vendor Scripts */}
           <script src='assets/js/modernizr.custom.js'></script>
           <script src='assets/js/jquery.isotope.min.js'></script>
           <script src='assets/js/jquery.magnific-popup.min.js'></script>
-          <script src='assets/js/animate.js'></script>
+          <script src='assets/js/animate.js'></script> */}
           <NextScript />
         </body>
       </Html>
