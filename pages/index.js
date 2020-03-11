b25d841859a90efdb0a2cf9b206433811224fea8
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-11 01:53:49 -0300

    add cards and jsx styles

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
 
   return (
     <Layout>
-      <Fragment>
+      <div>
         <Slider {...settings}>
           <div>
             <img
@@ -43,7 +43,7 @@ const Home = () => {
             <div className='row'>
               <div className='col-md-12'>
                 <div className='aligncenter'>
-                  <h2 className='aligncenter'>Soluciones</h2>
+                  <h2 className='aligncenter'>Tarifas</h2>
                   Nos especializamos en transportes marítimo, aéreos y
                   terrestre, carga de proyectos específicos y su logística. Esto
                   lo hacemos a través de un equipo humano y profesional
@@ -55,84 +55,247 @@ const Home = () => {
             </div>
 
             <div className='row service-v1 margin-bottom-40'>
-              <div className='col-md-3 md-margin-bottom-40'>
-                <div className='card small'>
-                  <div className='card-image'>
-                    <img
-                      className='img-responsive'
-                      src='images/service1.jpg'
-                      alt=''
-                    />
-                    <span className='card-title'>Transporte Maritimo</span>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
                   </div>
-                  <div className='card-content'>
-                    <p className='text-center'>
-                      Amplia experiencia en transporte marítimo internacional de
-                      cargas.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className='col-md-3 md-margin-bottom-40'>
-                <div className='card small'>
-                  <div className='card-image'>
-                    <img
-                      className='img-responsive'
-                      src='images/service2.jpg'
-                      alt=''
-                    />
-                    <span className='card-title'>
-                      &nbsp;&nbsp;Transporte Aereo&nbsp;&nbsp;
-                    </span>
-                  </div>
-                  <div className='card-content'>
-                    <p className='text-center'>
-                      Diseñamos soluciones flexibles mediante transporte aereo
-                      de carga.
-                    </p>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
                   </div>
                 </div>
               </div>
-              <div className='col-md-3 md-margin-bottom-40'>
-                <div className='card small'>
-                  <div className='card-image'>
-                    <img
-                      className='img-responsive'
-                      src='images/service3.jpg'
-                      alt=''
-                    />
-                    <span className='card-title'>Transporte Terretre</span>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
                   </div>
-                  <div className='card-content'>
-                    <p className='text-center'>
-                      Transporte de carga via terrestre a nivel nacional como
-                      internacional.
-                    </p>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
                   </div>
                 </div>
               </div>
-              <div className='col-md-3 md-margin-bottom-40'>
-                <div className='card small'>
-                  <div className='card-image'>
-                    <img
-                      className='img-responsive'
-                      src='images/service4.jpg'
-                      alt=''
-                    />
-                    <span className='card-title'>Cargas de Proyecto</span>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
                   </div>
-                  <div className='card-content'>
-                    <p className='text-center'>
-                      Diseñamos e implementamos soluciones para diferentes
-                      industrias.
-                    </p>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
+                  </div>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
+                  </div>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
+                  </div>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
+                  </div>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-3 md-margin-bottom-40 text-center'>
+                <div className='card'>
+                  <div className='card-header'>
+                    <h4 className='my-0 font-weight-normal'>Free</h4>
+                  </div>
+                  <div className='card-body'>
+                    <h1 className='card-title pricing-card-title'>
+                      $15 <small className='text-muted'>/ mo</small>
+                    </h1>
+                    <ul className='list-unstyled mt-3 mb-4'>
+                      <li>20 users included</li>
+                      <li>10 GB of storage</li>
+                      <li>Priority email support</li>
+                      <li>Help center access</li>
+                    </ul>
+                    <button
+                      type='button'
+                      className='btn btn-primary waves-effect waves-dark'
+                    >
+                      Get started
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </section>
-      </Fragment>
+        <style jsx>{`
+          .card {
+            position: relative;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-direction: column;
+            flex-direction: column;
+            min-width: 0;
+            word-wrap: break-word;
+            background-color: #fff;
+            background-clip: border-box;
+            border: 1px solid rgba(0, 0, 0, 0.125);
+            border-radius: 0.25rem;
+          }
+
+          .card-header {
+            padding: 0.75rem 1.25rem;
+            margin-bottom: 0;
+            background-color: rgba(0, 0, 0, 0.03);
+            border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+          }
+
+          .card-header h4 {
+            font-size: 2rem;
+            margin-bottom: 10px;
+          }
+
+          .card-body {
+            flex: 1 1 auto;
+            padding: 1.25rem;
+          }
+
+          .card-body .card-title {
+            text-transform: none !important;
+            font-size: 3.5rem;
+            margin-top: 0;
+            margin-bottom: 1.5rem;
+          }
+
+          ul,
+          ol {
+            margin-top: 0;
+            margin-bottom: 1.6rem;
+          }
+        `}</style>
+      </div>
     </Layout>
   );
 };
