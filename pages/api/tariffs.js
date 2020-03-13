0acb664a73a8d3691b1103074dc5c66e968f59e0
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2020-03-13 02:24:58 -0300

    tariff api

---
+++ pages/api/tariffs.js
@@ -0,0 +1,17 @@
+import nextConnect from 'next-connect';
+import middleware from '../../middlewares/database';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  let doc = await req.db
+    .collection('tariffs')
+    .find({})
+    .toArray();
+  console.log(doc);
+  res.json(doc);
+});
+
+export default handler;
