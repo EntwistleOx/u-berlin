2eb9c2261b19f78c57e91cb10d58b6b04ef9810a
Juan Diaz <juan.diaz.12069@gmail.com>
2020-05-27 16:08:23 -0400

    move up custom css

--- components/Layout.js
+++ components/Layout.js
@@ -15,13 +15,13 @@ const Layout = (props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="U-Berlin Logistics" />
         <title>U-Berlin Logistics</title>
+        <link href="css/styles.css" rel="stylesheet" />
         <link
           rel="stylesheet"
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
           crossOrigin="anonymous"
         />
-        <link href="css/styles.css" rel="stylesheet" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <Topbar />
